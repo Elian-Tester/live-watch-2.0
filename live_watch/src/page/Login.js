900a646ce366d0355df6ac0f7c8168c92733556b
Kevin-KHA <201378@ids.upchiapas.edu.mx>
2021-07-01 21:20:35 -0500

    Login + Css

--- live_watch/src/page/Login.js
+++ live_watch/src/page/Login.js
@@ -1,16 +1,37 @@
 import React from "react";
-import css from '../assets/css/header.css'
+import css from '../assets/css/login.css'
 import {Link} from "react-router-dom";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faSignInAlt,faUserSecret} from '@fortawesome/free-solid-svg-icons'
 class Login  extends  React.Component
 {
     render()
     {
         return(
-            <div>
-                <h1>login</h1>
-            </div>
+                <body className="">
+                    <div className="modal-dialog text-center">
+                        <h2>Bienvenido a Live-Watch</h2>
+                        <div className="col-sm-12 main-section">
+                            <div className="modal-content">
+                                <div className="col-12 user-img">
+                                    <img src="src/image/bob.png" width={80} alt=""/>
+                                </div>
+                                <form className="col-12" >
+                                        <div className="form-group" id="user-group">
+                                            <label className="form-group ">Dinos tu nombre: </label>
+                                            <FontAwesomeIcon icon={faUserSecret} className="ni"/>
+                                                <input type="text" className=" form-control" placeholder="Usuario..." />
+                                        </div>
+                                    <div className="d-grid gap-2 form-group">
+                                        <button className="btn  entrar" type="submit">
+                                            <FontAwesomeIcon icon={faSignInAlt}/> Ingresar</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
 
-
+                    </div>
+                </body>
         )
     }
 }
--- live_watch/webpack.config.js
+++ live_watch/webpack.config.js
@@ -21,6 +21,17 @@ module.exports = {
             { //Regla para trabajar con CSS
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
+            },
+            { //Regla para trabajar con imágenes
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'image/[name].[ext]'
+                        }
+                    },
+                ],
             }
         ],
     },
